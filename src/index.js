ef21eaa953cb443cd67e442df0debb4ba5432c92
Aléxia <133011626+AlexiaDiasB@users.noreply.github.com>
2023-08-25 16:29:24 +0000

    enviando

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//index.js
+import dotenv from "dotenv";
+import express from "express";      // Requisição do pacote do express
+dotenv.config();
+import roteadorUsuario from "./routes/usuario.js";
+
+
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta
+
+//"um trem que acontece no meio" - Porto, Arthur 
+app.use(express.json());
+app.use(roteadorUsuario);
+
+app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+  res.json({
+    message: "API para CRUD usuario: link_gitgub",  
+  });
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
