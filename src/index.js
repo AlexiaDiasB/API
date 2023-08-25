ca43c7c99ad0e416d12b22ebab74950cfb9b5050
Aléxia <133011626+AlexiaDiasB@users.noreply.github.com>
2023-08-25 17:13:14 +0000

    att 15

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //index.js
+import roteadorLogin from "./routes/login.js";
 import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
 dotenv.config();
@@ -10,6 +11,7 @@ const port = 3000;                  // Define a porta
 //"um trem que acontece no meio" - Porto, Arthur 
 app.use(express.json());
 app.use(roteadorUsuario);
+app.use(roteadorLogin);
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
